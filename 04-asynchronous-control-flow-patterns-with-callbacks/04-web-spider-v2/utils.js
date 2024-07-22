1871079437c526ec7d7e616fab068865dbd80667
apmJSW <jeongsw@apm-coin.com>
2024-07-22 17:25:25 +0900

    feat: web-spider-v2 update

--- 04-asynchronous-control-flow-patterns-with-callbacks/04-web-spider-v2/utils.js
+++ 04-asynchronous-control-flow-patterns-with-callbacks/04-web-spider-v2/utils.js
@@ -38,3 +38,18 @@ export function getPageLinks (currentUrl, body) {
     })
     .filter(Boolean)
 };
+
+export const iterateSeries = (collection, iteratorCallback, finalCallback) => {
+  const iterate = (index) => {
+    if (index === collection.length) {
+      return finalCallback();
+    }
+    iteratorCallback(collection[index], (err) => {
+      if (err) {
+        return finalCallback(err);
+      }
+      iterate(index + 1);
+    });
+  }
+  iterate(0)
+}
