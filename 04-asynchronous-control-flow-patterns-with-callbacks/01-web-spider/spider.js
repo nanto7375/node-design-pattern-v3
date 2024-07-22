9941b60acf220517ccc88bb7ab9858f66ebc1e61
apmJSW <jeongsw@apm-coin.com>
2024-07-22 17:28:40 +0900

    feat: web-spider-v2 update

--- 04-asynchronous-control-flow-patterns-with-callbacks/01-web-spider/spider.js
+++ 04-asynchronous-control-flow-patterns-with-callbacks/01-web-spider/spider.js
@@ -7,29 +7,20 @@ import { urlToFilename } from './utils.js'
 export function spider (url, cb) {
   const filename = urlToFilename(url)
   fs.access(filename, err => { // [1]
-    if (err && err.code === 'ENOENT') {
-      console.log(`Downloading ${url} into ${filename}`)
-      superagent.get(url).end((err, res) => { // [2]
-        if (err) {
-          cb(err)
-        } else {
-          mkdirp(path.dirname(filename), err => { // [3]
-            if (err) {
-              cb(err)
-            } else {
-              fs.writeFile(filename, res.text, err => { // [4]
-                if (err) {
-                  cb(err)
-                } else {
-                  cb(null, filename, true)
-                }
-              })
-            }
-          })
-        }
-      })
-    } else {
-      cb(null, filename, false)
+    if (!err || err.code !== 'ENOENT') {
+      return cb(null, filename, false)
     }
+
+    console.log(`Downloading ${url} into ${filename}`)
+    superagent.get(url).end((err, res) => { // [2]
+      if (err) return cb(err)
+      mkdirp(path.dirname(filename), err => { // [3]
+        if (err) return cb(err)
+        fs.writeFile(filename, res.text, err => { // [4]
+          if (err) return cb(err)
+          cb(null, filename, true)
+        })          
+      })
+    })
   })
 }
