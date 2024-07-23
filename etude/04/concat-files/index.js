9b8f17881d8b4c95dcd65e288943d8190201b191
seungwoo <jsu7375@gmail.com>
2024-07-24 00:09:18 +0900

    text: README 작성

--- etude/04/concat-files/index.js
+++ etude/04/concat-files/index.js
@@ -1,14 +1,6 @@
 import fs from 'fs'
 import { FileConcator } from './concat-files.js'
 
-// 4-1 파일 연결 (callback style)
-// 1. file 모두 읽기
-// 2. 읽은 파일의 내용들 합치기
-// 3. destination파일에 저장하기
-// *파일 목록 순서대로 내용을 합쳐야 한다
-
-// const [,, destination, ...files] = process.argv
-
 class DataQueue {
   queue = []
 
